d0bbb976418d71afa94172f53445a46417b1bec5
Abel Zambrana <abel.led94@gmail.com>
2024-08-19 17:17:56 -0300

    Se agrega linea de codigo en archivo productos.js

---
+++ js/productos.js
@@ -0,0 +1,5 @@
+const baseURLProductos = 'http://127.0.0.1:5000/api/productos/';  
+
+document.addEventListener('DOMContentLoaded', () => {
+    cargarProductos(); // Cargar productos por defecto al iniciar la página
+});
