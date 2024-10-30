e41e5297bfb95961d4fcc983c6bc5c88d90254b7
Abel Zambrana <abel.led94@gmail.com>
2024-10-30 19:19:29 -0300

    se cambia el tipo a id en linea 25 de producto.js

--- js/productos.js
+++ js/productos.js
@@ -22,7 +22,7 @@ function mostrarProductos(productos) {
     mainContent.innerHTML = `
         <h2>Gestión de Productos</h2>
         <form id="form-producto">
-            <input type="hidden" id="id" name="id">
+            <input type="number" id="id" name="id">
 
             <input type="text" id="nombre" name="nombre" placeholder="Nombre">
 
